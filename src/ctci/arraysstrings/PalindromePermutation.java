d19be76628ab0b08daec1b52a34d29aee00aebfb
mike <mike@mike-kneeputer>
2016-12-23 11:21:52 -0600

    palindrome permutation with bit manipulation book answer

--- src/ctci/arraysstrings/PalindromePermutation.java
+++ src/ctci/arraysstrings/PalindromePermutation.java
@@ -7,9 +7,9 @@ import tutes.util.ds.HashTable;
 class PalindromePermutation {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-
 		String input = in.nextLine();
-		System.out.println(isPalindrome(input));
+//		System.out.println(isPalindrome(input));
+		System.out.println(isPermutationPalindrome(input));
 		in.close();
 	}
 
@@ -46,4 +46,48 @@ class PalindromePermutation {
 		}
 		return true;
 	}
+	
+	//BOOK ANSWER-------------------------------------------------
+	private static boolean isPermutationPalindrome(String phrase){
+		int bitVector = createBitVector(phrase);
+		return bitVector == 0 || checkExactlyOneBitSet(bitVector);
+	}
+	
+	private static int createBitVector(String phrase){
+		int bitVector = 0;
+		for(char c : phrase.toCharArray()){
+			int x = getCharNumber(c);
+			bitVector = toggle(bitVector, x);
+		}
+		return bitVector;
+	}
+	
+	private static int toggle(int bitVector, int index){
+		if(index < 0) return bitVector;
+		
+		int mask = 1 << index;
+		//book had the commented out section, but I feel like XORing works and is simpler?
+		bitVector ^= mask;
+//		if((bitVector & mask) == 0){
+//			bitVector |= mask;
+//		} else {
+//			bitVector &= ~mask;
+//		}
+		return bitVector;
+	}
+	
+	private static boolean checkExactlyOneBitSet(int bitVector){
+		return (bitVector & (bitVector - 1)) == 0;
+	}
+	
+	private static int getCharNumber(Character c){
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		int val = Character.getNumericValue(c);
+		if(a <= val && val <= z){
+			return val - a;
+		}
+		return -1;
+	}
+	//BOOK ANSWER-------------------------------------------------
 }
