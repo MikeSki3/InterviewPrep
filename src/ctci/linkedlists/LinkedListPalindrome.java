af9d936e2f8b48e6c9e231e82e4ccc2b09218f75
mike <mike@mike-kneeputer>
2016-12-23 19:55:10 -0600

    sill working on linked list palindrome

--- src/ctci/linkedlists/LinkedListPalindrome.java
+++ src/ctci/linkedlists/LinkedListPalindrome.java
@@ -1,12 +1,30 @@
 package ctci.linkedlists;
 
 import tutes.util.ds.LinkedList;
+import tutes.util.ds.LinkedListNode;
 
 public class LinkedListPalindrome {
 	public static void main(String[] args) {
 		LinkedList<Character> list = new LinkedList<>('a');
 		populateList(list);
-		
+		System.out.println(isPalindrome(list));
+	}
+
+	private static boolean isPalindrome(LinkedList<Character> list) {
+		return isPalindrome(list.getHead(), list.getHead(), 0, list.size());
+	}
+
+	//need to figure out a better way to get to the middle and then work out from there
+	private static boolean isPalindrome(LinkedListNode<Character> prev, LinkedListNode<Character> next, int currIndex, int size) {
+		int middle = size / 2;
+		if(currIndex == middle){
+			if(size % 2 != 0){
+				
+			}
+		} else {
+			return isPalindrome(prev.getNext(), next.getNext(), ++currIndex, size);
+		}
+		return false;
 	}
 
 	private static void populateList(LinkedList<Character> list) {
